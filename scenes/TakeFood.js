978f3ec4a96113c838312ae6b8c5269ba196c6b8
dezisque <=>
2020-06-19 19:31:54 +0300

    push

--- scenes/TakeFood.js
+++ scenes/TakeFood.js
@@ -7,35 +7,24 @@ new (class TakeFood extends Scene {
     super.struct = {
       on: [
         ["text", this.onText],
-        ["photo", this.onPhoto]
       ],
-      start: [[this.main]],
-      enter: [[this.zahod]]
+      enter: [[this.enter]]
     };
   }
-  main(ctx) {
-    ctx.reply("Main");
+  enter(ctx) {
+    ctx.reply(
+        "Выберите интересующий товар",
+        Markup.keyboard(["Назад"])
+            .oneTime()
+            .resize()
+            .extra()
+    );
   }
   onText(ctx) {
     switch (ctx.message.text) {
       case "Назад":
         ctx.scene.enter("Main");
         break;
-      default:
-        ctx.reply("Wrong!");
-        break;
     }
-  }
-  onPhoto(ctx) {
-    ctx.reply("Вау");
-  }
-  async zahod(ctx) {
-    await ctx.reply(
-      'Вы зашли в раздел "Взять еду"',
-      Markup.keyboard(["Назад"])
-        .oneTime()
-        .resize()
-        .extra()
-    );
   }
 })();
