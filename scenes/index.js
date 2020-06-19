da8f4e5e0afba35c3ee4c6cdb90f9c23929f1eca
Dimitry Bychkov <interferstd@yandex.ru>
2020-06-19 15:46:30 +0300

    auto path

--- scenes/index.js
+++ scenes/index.js
@@ -1,6 +1,7 @@
-if (global.ScenesController === undefined)
-  ["Main", "Configuration", "GiveFood", "TakeFood"].map(name => {
-    require("./" + name);
-  });
+async function initScenes(path) {
+  const dir = await require("fs").promises.opendir(path);
+  for await (const dirent of dir) require("../" + path + dirent.name);
+}
+if (global.ScenesController === undefined) initScenes("./scenes/");
 
 module.exports = require("./Scenes");
