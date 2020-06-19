09431668ac757d5c2b9acce3f34a50102f3ea740
Dimitry Bychkov <interferstd@yandex.ru>
2020-06-19 14:39:49 +0300

    fix

--- scenes/GiveFood.js
+++ scenes/GiveFood.js
@@ -1,27 +1,27 @@
 const { Scene } = require("./Scenes");
 
 new (class GiveFood extends Scene {
-    constructor() {
-        super("GiveFood");
-        super.struct = {
-            on: [
-                ["text", this.onText],
-                ["photo", this.onPhoto]
-            ],
-            start: [[this.main]],
-            enter: [[this.zahod]]
-        };
-    }
-    main(ctx) {
-        ctx.reply("Main");
-    }
-    onText(ctx) {
-        ctx.reply("Чё");
-    }
-    onPhoto(ctx) {
-        ctx.reply("Вау");
-    }
-    async zahod(ctx) {
-        await ctx.reply("Зашли в раздел \"Отдать еду\"");
-    }
+  constructor() {
+    super("GiveFood");
+    super.struct = {
+      on: [
+        ["text", this.onText],
+        ["photo", this.onPhoto]
+      ],
+      start: [[this.main]],
+      enter: [[this.zahod]]
+    };
+  }
+  main(ctx) {
+    ctx.reply("Main");
+  }
+  onText(ctx) {
+    ctx.reply("Чё");
+  }
+  onPhoto(ctx) {
+    ctx.reply("Вау");
+  }
+  async zahod(ctx) {
+    await ctx.reply('Зашли в раздел "Отдать еду"');
+  }
 })();
