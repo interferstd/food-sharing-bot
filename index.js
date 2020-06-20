5faa596abffc730baf2cd33d7a87fe59886cc24a
Dimitry Bychkov <interferstd@yandex.ru>
2020-06-21 02:55:18 +0300

    many vk share

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const {
   ScenesController
 } = require("./scenes");
 
-const { telegram, vk_token } = require("./config.json");
+const { telegram, vk_token, foodshare } = require("./config.json");
 global.bot = new Telegraf(telegram);
 const vk = require("./vk.js").get(vk_token);
 
@@ -21,7 +21,7 @@ global.bot.start(ctx => ctx.scene.enter("Start"));
 global.Controller.on("DataBaseConnected", async () => {
   await global.bot.launch();
   setInterval(() => {
-    vk.getPosts("sharingfood", 10);
+    foodshare.map(name => vk.getPosts(name, 10));
   }, 60000);
   console.log("Listening...");
 });
